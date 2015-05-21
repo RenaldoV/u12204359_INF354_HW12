a6d5e1bdf40af4d8a33efd4ac0ef289ff1b41a66
RenaldoV <u12204359@tuks.co.za>
2015-05-21 09:18:17 +0200

    Added PHP script
    Name and student number in oho script

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	echo "<!DOCTYPE html>";
+	echo "<html>";
+	echo "<head>";
+	echo "<meta charset=utf-8 />";
+	echo "<title>Andreas & Renaldo</title>";
+	echo "</head>";
+	echo "<body>";
+	echo "<h1>Renaldo van Dyk</h1>";
+	echo "<h2>12204359</h2>";
+
+
+
+	echo "</body>";
+	echo "</html>";
+	
+?>
